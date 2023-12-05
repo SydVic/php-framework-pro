cb75fc64e2f4b7ca0e19c0cb4f5094cc79f57a1b
SydVic <fabrizio.bocca@gmail.com>
2023-12-05 15:15:28 +0100

    implementazione redirect response

---
+++ framework/src/Http/NotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SydVic\Framework\Http;
+
+class NotFoundException extends HttpException
+{
+
+}
--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -6,10 +6,13 @@ use App\Entity\Post;
 use App\Repository\PostMapper;
 use App\Repository\PostRepository;
 use SydVic\Framework\Controller\AbstractController;
+use SydVic\Framework\Http\RedirectResponse;
 use SydVic\Framework\Http\Response;
 
 class PostsController extends AbstractController
 {
+    private string $createRoute = '/posts/create';
+
     public function __construct(
         private PostMapper $postMapper,
         private PostRepository $postRepository
@@ -19,7 +22,7 @@ class PostsController extends AbstractController
 
     public function show(int $id): Response
     {
-        $post = $this->postRepository->findById($id);
+        $post = $this->postRepository->findOrFail($id);
 
         return $this->render('/posts/show.html.twig', [
             'post' => $post
@@ -28,10 +31,10 @@ class PostsController extends AbstractController
 
     public function create(): Response
     {
-        return  $this->render('/posts/create.html.twig');
+        return  $this->render("$this->createRoute.html.twig");
     }
 
-    public function store(): void
+    public function store(): Response
     {
         $title = $this->request->postParams['title'];
         $body = $this->request->postParams['body'];
@@ -41,6 +44,6 @@ class PostsController extends AbstractController
 
         $this->postMapper->save($post);
 
-        dd($post);
+        return new RedirectResponse($this->createRoute);
     }
 }
